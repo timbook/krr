baacfec735fbd2ce49e7e599fca9482acd5c390c
tim.book <tim.book@summitllc.us>
2016-12-02 12:41:54 -0500

    Initial commit.  Hello world!

---
+++ R/modelPrediction.R
@@ -0,0 +1,39 @@
+#' Predict new krr outcomes.
+#'
+#' A function for making predictions based on a krr (kernel ridge regression)
+#' model.
+#'
+#' @param krr_mod A model produced by the \code{krr} function
+#' @param xnew New quantitative explanatory variables to predict (vector,
+#' matrix, or data.frame).
+#' @param ynew Optional new response variables, helps compute MSE.
+#'
+#' @return Predicted values and MSE.
+#'
+#' @export
+krr_pred <- function(krr_mod, xnew, ynew = NA) {
+  xnew <- as.matrix(xnew)
+  nnew <- nrow(xnew)
+  pnew <- ncol(xnew)
+  x <- krr_mod$x
+  n <- nrow(x)
+
+  ker <- krr_mod$ker
+
+  # K_tilde <- makeGram(xnew, x, ker)
+
+  K_tilde <- kernlab::kernelMatrix(ker, xnew, x)
+
+  f_tilde <- K_tilde %*% krr_mod$alpha_hat
+
+  if (is.numeric(ynew)) {
+    MSE <- mean((f_tilde - ynew)^2)
+  } else {
+    MSE <- NA
+  }
+
+  return(list(
+    "pred" = f_tilde,
+    "MSE" = MSE
+  ))
+}

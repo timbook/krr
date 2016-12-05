b9e6b812c8026e4205b20ffe6c5f4a01b8161c7b
tim.book <tim.book@summitllc.us>
2016-12-05 16:48:16 -0500

    Roxygenized S3 methods, added to namespace.

--- R/classMethods.R
+++ R/classMethods.R
@@ -1,18 +1,45 @@
-# TODO: Roxygenize these
+#' Produce fitted values
+#'
+#' @param krrm A model of type krr.
+#'
+#' @return Fitted values
+#'
+#' @export
 fitted.krr <- function(krrm) krrm$pred
 
+#' Produce residuals
+#'
+#' @param krrm A model of type krr.
+#'
+#' @return Residuals
+#'
+#' @export
 resid.krr <- function(krrm) krrm$residuals
 
+#' Compute MSE
+#'
+#' @param krrm A model of type krr.
+#'
+#' @return Fitted values
+#'
+#' @export
 MSE <- function(x) UseMethod("MSE", x)
 MSE.krr <- function(krrm) krrm$MSE
 
+#' Produce fitted value plot.
+#'
+#' @param krrm A model of type krr.
+#'
+#' @return An aesthetic plot.
+#'
+#' @export
 plot.krr <- function(krrm) {
   x <- as.matrix(krrm$x)
   p <- ncol(x)
   if(p != 1) {
     stop("ERROR: Can only plot univariate predictors!")
   }
-  
+
   y <- krrm$pred - krrm$residuals
   df <- data.frame(x, y, yhat = krrm$pred)
   df <- df[order(df$x),]
